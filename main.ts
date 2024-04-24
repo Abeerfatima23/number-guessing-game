5db4a16e0c4a4ec783bc47d32f7b1b2be7fb1039
abeer fatima <abeerfatimahom@gmail.com>
2024-04-24 20:54:11 +0500

    number guessing game

---
+++ main.ts
@@ -0,0 +1,22 @@
+#! usr/bin/env node(shabang)
+
+import inquirer from "inquirer";
+
+console.log("  Welcome To Abeer NUMBER-GUESSING GAME ")
+
+const randomnumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name:"UserGuessedNumber",
+        type:"number",
+        message:"Enter Your Guessed Number(Number Limit From 1 to 5)"
+    }  
+]);
+
+if(answer.UserGuessedNumber === randomnumber){
+    console.log("CONGRATULATION! You Guessed A Correct Number");
+}
+else{
+    console.log("OPPS! You Lost");
+}
